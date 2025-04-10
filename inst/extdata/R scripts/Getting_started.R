e5c980644bd7c083c843a7a09c92e3d0cbebd477
Arthfael <armel.nicolas@gmail.com>
2025-04-10 17:40:28 +0200

    Add files via upload

---
+++ inst/extdata/R scripts/Getting_started.R
@@ -0,0 +1,20 @@
+# Useful commands
+
+# To clean the previous version cached by pak
+#a <- pak::cache_summary()
+#cat(a$cachepath, "\n")
+#pak::cache_delete("proteoCraft")
+
+# To install the latest version
+unloadNamespace("proteoCraft")
+remove.packages("proteoCraft")
+pak::pkg_install("Arthfael/proteoCraft", upgrade = TRUE, ask = FALSE)
+# or
+devtools::install_github("Arthfael/proteoCraft", upgrade = TRUE)
+
+# Load the package
+library(proteoCraft)
+
+# It is always a good idea to configure the package before a fresh installation,
+# or even after any update: this will move the latest analysis workflows to your temporary analysis folder
+proteoCraft::Configure()
