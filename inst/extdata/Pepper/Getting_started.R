251fac8420b69792abd366a37de5faa037b05983
Arthfael <armel.nicolas@gmail.com>
2025-09-08 11:14:57 +0200

    Add files via upload

---
+++ inst/extdata/Pepper/Getting_started.R
@@ -0,0 +1,27 @@
+# Useful commands
+
+# To clean the previous version cached by pak
+if (!require(pak)) {
+  install.packages("pak")
+}
+require(pak)
+#a <- pak::cache_summary()
+#cat(a$cachepath, "\n")
+#pak::cache_delete("proteoCraft")
+
+# To install the latest version
+try({
+  unloadNamespace("proteoCraft")
+  remove.packages("proteoCraft")
+}, silent = TRUE)
+pak::pkg_install("Arthfael/proteoCraft", upgrade = TRUE, ask = FALSE)
+# Alternative way to install:
+#devtools::install_github("Arthfael/proteoCraft", upgrade = TRUE)
+
+# Load the package
+library(proteoCraft)
+
+# It is always a good idea to configure the package before a fresh installation,
+# or even after any update: this will move a copy of the latest analysis workflows to your temporary analysis folder
+proteoCraft::Configure()
+#proteoCraft::load_Bckp()
